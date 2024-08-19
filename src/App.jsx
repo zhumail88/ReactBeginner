f39ac78ddd537402d43d9586e94f9753c7fbd43c
zhumail88 <shumailtassadaq3@gmail.com>
2024-08-19 21:48:43 +0500

    Card Details Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ let CardDetails = () => {
           description={"I’m currently grinding through my Bachelors in Edge Maxing. I'm out here mastering the art of Skibidi and flexing my top-tier GYAT skills. Catch me in the code zone, slaying bugs and boosting my rizz like a true GYAT rizzler."} />
 
         <Cards
-          img={"src/assets/bobzy.jpg"}
+          img={"src/assets/zhumail.jpg"}
           name={"Bobzy King"}
           description={"I am currently pursuing a Bachelors degree in Yapology and am in my 15th semester in the prestigious institute of Rizzlers University. I am deeply influnced by my great teacher Mr. Andrew Emery Tate The Third."} />
 
@@ -39,9 +39,9 @@ let CardDetails = () => {
           description={"Skibidi gyatt rizz only in ohio duke dennis did you pray today livvy dunne rizzing up baby gronk sussy imposter pibby glitch in real life sigma alpha omega male grindset andrew tate."} />
 
         <Cards
-          img={"src/assets/zafar.jpg"}
+          img={"src/assets/zhumail.jpg"}
           name={"Zafar Bhai"}
-          description={"This nigga finna come to your home but his bag to the side, bend you over and give you react tutorials. And probabaly fuck you afterwards."} />
+          description={"This nigga finna come to your home but his bag to the side, bend you over and give you react tutorials.."} />
       </div>
 
       <div className="selectBoxArea">
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -9,7 +9,6 @@ export default function Cards({img, name, description }) {
       <p>{description}</p>
     </div>
 
-
   )
 }
 
